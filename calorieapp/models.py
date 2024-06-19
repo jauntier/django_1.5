6fb8b385aaa2238675fa4c46f4d5663642b26086
jauntier <nderituadrian0@gmail.com>
2024-06-19 12:37:53 +0300

    The calorie counter website

---
+++ calorieapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+from django.utils import timezone
+
+class FoodItem(models.Model):
+    name = models.CharField(max_length=100)
+    calories = models.PositiveIntegerField()
+    added_on = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
